857c9e09cfe6f7b6878589258a7c5ca6f245eaac
Faizan Chaudary <83314898+faizan-debug@users.noreply.github.com>
2024-04-28 14:27:54 +0500

    React started
    Home and About page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import React, {Fragment} from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Home from './components/layout/pages/Home';
+import About from './components/pages/About'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom/client'
+
 
 const App = () => {
   return (
-    <div className="App">
-      Contact Management App
-    </div>
+    <Router>
+    <Fragment className="App">
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route exact path = '/' Component={Home}/>
+          <Route exact path = '/about' Component={About}/>
+        </Routes>
+      </div>
+    </Fragment>
+    </Router>
   );
 }
 
